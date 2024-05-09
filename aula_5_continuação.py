99d876ed19caf1a8709e4b0cf4b07c8174ba18b6
eliel1995 <168238322+eliel1995@users.noreply.github.com>
2024-05-08 21:42:21 -0300

    Add files via upload

---
+++ aula_5_continuação.py
@@ -0,0 +1,64 @@
+# **Exercício 1:** Escreva um programa que use a função `range()` para gerar os números pares de 2 a 20 e, em seguida, imprima cada número. Utilize os métodos que vimos até então. 
+
+# numeros = list(range(2,21,2))
+# print(numeros)
+
+# **Exercício 2:** Escreva um programa que use a função `range()` para gerar os múltiplos de 5 em 5 ate 50 e, em seguida.
+
+# numeros = list(range(0,51,5))
+# print(numeros)
+
+
+# **Exercício 3:** Escreva um programa que use a função `type()` para verificar o tipo de uma variável.
+
+# nome = input('Digite seu nome >>')
+# idade = int(input("Digite sua idade"))
+
+# print(nome)
+# print(type(nome))
+# print(idade)
+# print(type(idade))
+
+# **Exercício 4:** Escreva um programa que use a função `print()` para imprimir uma mensagem de saudação personalizada, incluindo o nome do usuário.
+
+# nome = input('digite seu nome >>')
+# saudacao = 'Seja bem vindo (a)'
+
+# print(saudacao,nome)
+
+
+# Exercício 5: Escreva um programa que use a função range() para gerar os números ímpares de 1 a 10 e, em seguida, calcule e imprima a média desses números.
+
+# numeros = list(range(1,10,2))
+# qtde_numeros = len(numeros)
+# soma = sum(numeros)
+
+# media = soma / qtde_numeros
+
+# print(media)
+
+# Exercício 6: Descubra o máximo e o mínimo da sequência criada no  exercício 5
+
+# maximo = max(numeros)
+# minimo = min(numeros)
+
+# print(maximo, ",",minimo) #para funcionar tem que "descomentar linha 33 do exercicio 5"
+
+
+# Exercício 7: Ordene a lista  -  [10,56,40,5,4,9,7,1,0,56]
+
+# numeros = [10,56,40,5,4,9,7,1,0,56]
+
+# numeros_ordenados = sorted(numeros)
+
+# print(numeros_ordenados)
+
+
+# Exercícios 8:  some todos os valores da lista ordenada no exercício 7
+
+# numeros = [10,56,40,5,4,9,7,1,0,56]
+
+# soma = sum(numeros)
+
+# print(soma)
+
